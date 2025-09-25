65185529b032a586d81410af01e6aff61eea800d
Peter Lantz <djnex84@gmail.se>
2025-09-25 15:43:20 +0200

    chore: remove old project contents

--- tests/test_transform.py
+++
@@ -1,7 +0,0 @@
-from etl.transform import to_float
-
-def test_to_float():
-    assert to_float(1) == 1.0
-    assert to_float('5') == 5.0
-    assert to_float('hello') is None
-    assert to_float(None) is None
